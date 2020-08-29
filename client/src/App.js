81a8f35345efa690597bc334f69de05d5e3feeea
Saular Moaddeli <jake.moaddeli@gmail.com>
2020-08-29 13:06:19 -0700

    fixed some packages, ran tests, have to style

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/Navbar";
 import Header from "./components/Header";
 import Wrapper from "./components/Wrapper";
 
 import NoMatch from "./pages/NoMatch";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
-
-import "./App.css";
 
 class App extends Component {
  
--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -8,7 +8,7 @@ mongoose.connect(
     "mongodb://localhost/googlebooks"
 );
 
-const bookSeed =
+const bookSeed = [
 {
     authors: ["Suzanne Collins"],
     description: "Set in a dark vision of the near future, a terrifying reality TV show is taking place. Twelve boys and twelve girls are forced to appear in a live event called The Hunger Games. There is only one rule: kill or be killed. When sixteen-year-old Katniss Everdeen steps forward to take her younger sister's place in the games, she sees it as a death sentence. But Katniss has been close to death before. For her, survival is second nature.",
@@ -16,7 +16,7 @@ const bookSeed =
     link: "http://books.google.com/books?id=sazytgAACAAJ&dq=title:The+Hunger+Games&hl=&source=gbs_api",
     title: "The Hunger Games",
 }
-
+]
 
 db.Book
     .remove({})
